52c05442b6f24505e4a75fd4fe5e7d7428be1de3
Dzmitry Baryshau <dima.baryshev@live.com>
2023-11-13 09:41:02 +0300

    add pre-commit

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -4,12 +4,9 @@ import {
   PreloadedState,
 } from '@reduxjs/toolkit';
 import { userReducer } from 'entities/User';
+import { counterReducer } from 'entities/Counter';
 
-// import { counterReducer } from 'entities/Counter/model/slice/counterSlice';
-
-import { counterReducer } from '../../../../entities/Counter/model/slice/counterSlice';
 import { StateSchema } from './StateSchema';
-// Create the root reducer independently to obtain the RootState type
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function setupStore(preloadedState?: PreloadedState<any>) {
